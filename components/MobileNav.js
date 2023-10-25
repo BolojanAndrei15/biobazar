9391078554f53a44ffa821d30b1176b23735aafe
Your Name <you@example.com>
2023-10-25 10:07:30 +0300

    add style to MobileNav

--- components/MobileNav.js
+++ components/MobileNav.js
@@ -7,20 +7,35 @@ import {
     SheetTrigger,
   } from "@/components/ui/sheet"
 import { Button } from "./ui/button"
+import { AlignLeft, Store } from "lucide-react"
+import Home from "@/app/page"
   
 function MobileNav() {
   return (
   <Sheet>
-    <SheetTrigger>Open</SheetTrigger>
+    <SheetTrigger>
+        <Button variant="outline" className="w-6 h-6 flex justify-center items-center">
+            <AlignLeft />
+        </Button>
+    </SheetTrigger>
     <SheetContent>
       <SheetHeader>
         <SheetTitle>Biobazar</SheetTitle>
       </SheetHeader>
       <div>
-      <ul className="flex flex-col space-x-3">
-        <li>Acasă</li>
-        <li>Biobazar</li>
-        <li>Despre</li>
+      <ul className="flex flex-col ">
+        <li className="flex space-x-2 items-center">
+        <Home />
+            <h1>Acasă</h1>
+            </li>
+            <li className="flex space-x-2 items-center">
+            <Store />
+            <h1>Biobazar</h1>
+            </li>
+            <li className="flex space-x-2 items-center">
+            <Store />
+            <h1>Biobazar</h1>
+            </li>
         <li>Contact</li>
       </ul>
 
