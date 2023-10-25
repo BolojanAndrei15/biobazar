4a57a3cdf81dec21798ca5d687451d8f9a94d6a4
Your Name <you@example.com>
2023-10-25 09:51:26 +0300

    add style to navbar and add elements

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,8 +2,19 @@ import React from "react";
 
 function Navbar() {
   return (
-    <nav>
+    <nav className="flex p-2 justify-between">
       <h1>Biobazar</h1>
+      <ul className="flex space-x-3">
+        <li>Acasă</li>
+        <li>Biobazar</li>
+        <li>Despre</li>
+        <li>Contact</li>
+      </ul>
+
+      <div>
+        <Button>Înregistrează-te ca producător</Button>
+        <Button>Autentificare</Button>
+      </div>
     </nav>
   )
 }
