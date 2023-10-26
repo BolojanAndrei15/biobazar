e0d4a2cc250a085a622e31ef98982ec43ef3133d
Your Name <you@example.com>
2023-10-26 14:33:58 +0300

    add the page layout of [categorie]

--- app/biobazar/[categorie]/page.js
+++ app/biobazar/[categorie]/page.js
@@ -1,7 +1,50 @@
-import React from "react";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { categories } from "@/categories";
+
 
 function Categorie() {
-  return <div>Categorie</div>;
+  return (
+    <div className="flex">
+      <div className="mx-20">
+        <Card className="w-full flex flex-col space-x-2">
+          <CardContent>
+            <Input />
+            <DropdownMenu>
+  <DropdownMenuTrigger>Selectează o categorie</DropdownMenuTrigger>
+  <DropdownMenuContent>
+    {
+      categories.map((category)=>{
+        return (<DropdownMenuItem>{category.title}</DropdownMenuItem>)
+      })
+    }
+  </DropdownMenuContent>
+</DropdownMenu>
+<DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline">Ordonează</Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
+          <DropdownMenuRadioItem value="ascending">Crescător</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="descending">Descrescător</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="latest">Cele mai noi</DropdownMenuRadioItem>
+        </DropdownMenuRadioGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  )
 }
 
 export default Categorie;
